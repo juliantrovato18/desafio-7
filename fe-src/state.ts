80ee1dcade8395b8227c3247b54b19e3abd93c51
juliantrovato <julian_todobien@hotmail.com>
2022-03-25 17:00:44 -0300

    recargar la pagina da problemas2

--- fe-src/state.ts
+++ fe-src/state.ts
@@ -2,8 +2,8 @@ import { Json } from "sequelize/dist/lib/utils";
 import { callbackify } from "util";
 import "dotenv/config";
 
-//const API_BASE_URL = "https://desafio-7.herokuapp.com";
-const API_BASE_URL = "http://localhost:3003"
+const API_BASE_URL = "https://desafio-7.herokuapp.com/";
+//const API_BASE_URL = "http://localhost:3003"
 
 const state = {
     data : {
